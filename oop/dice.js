8acd349e70a9304edc5857ea33627c2d670de033
Ryan Sobol <contact@ryansobol.com>
2017-05-10 15:20:08 -0700

    Tweak dice

--- oop/dice.js
+++ oop/dice.js
@@ -5,7 +5,7 @@ module.exports = {
     let total = 0;
 
     for (let i = 0; i < times; i++) {
-      total += Math.floor(Math.random() * sides) + 1;
+      total += Math.ceil(Math.random() * sides);
     }
 
     return total;
