a5e20691f1225212c46d967e37bd2533b106aefd
Ryan Sobol <contact@ryansobol.com>
2017-05-23 08:42:55 -0700

    Add removeDuplcatesSet and listAllAnagrams to Array

--- array/array.js
+++ array/array.js
@@ -1,6 +1,14 @@
 /* eslint-disable no-extend-native */
 'use strict';
 
+// O(2n) ~= O(n)
+Array.prototype.removeDuplcatesSet = function() {
+  const set = new Set(this);
+
+  return [...set];
+};
+
+// O(n)
 Array.prototype.removeDuplcatesInPlace = function() {
   if (this.length === 0) {
     return 0;
@@ -15,5 +23,33 @@ Array.prototype.removeDuplcatesInPlace = function() {
     }
   }
 
+  this.splice(i + 1);
+
   return i + 1;
+};
+
+// O(an) where a is the number of anagrams
+Array.prototype.listAllAnagrams = function() {
+  const table = {};
+
+  for (const word of this) {
+    const sortedWord = word.split('').sort().join('');
+
+    if (table[sortedWord]) {
+      table[sortedWord].push(word);
+    }
+    else {
+      table[sortedWord] = [word];
+    }
+  }
+
+  const results = [];
+
+  for (const sortedWord in table) {
+    if (table[sortedWord].length > 1) {
+      results.push(...table[sortedWord]);
+    }
+  }
+
+  return results;
 };
--- array/array.test.js
+++ array/array.test.js
@@ -5,18 +5,65 @@ const test = require('ava');
 require('./array');
 
 test('remove duplicates of sorted array in place', (t) => {
-  t.is([1].removeDuplcatesInPlace(), 1);
-  t.is([1, 1].removeDuplcatesInPlace(), 1);
-  t.is([1, 1, 1].removeDuplcatesInPlace(), 1);
-  t.is([1, 1, 1, 1].removeDuplcatesInPlace(), 1);
+  t.deepEqual([1].removeDuplcatesSet(), [1]);
+  t.deepEqual([1, 1].removeDuplcatesSet(), [1]);
+  t.deepEqual([1, 1, 1].removeDuplcatesSet(), [1]);
 
-  t.is([1, 1, 2].removeDuplcatesInPlace(), 2);
-  t.is([1, 1, 1, 2].removeDuplcatesInPlace(), 2);
+  t.deepEqual([1, 1, 2].removeDuplcatesSet(), [1, 2]);
+  t.deepEqual([1, 1, 1, 2].removeDuplcatesSet(), [1, 2]);
+  t.deepEqual([1, 2, 2].removeDuplcatesSet(), [1, 2]);
+  t.deepEqual([1, 2, 2, 2].removeDuplcatesSet(), [1, 2]);
 
-  t.is([1, 2, 2].removeDuplcatesInPlace(), 2);
-  t.is([1, 2, 2, 2].removeDuplcatesInPlace(), 2);
+  t.deepEqual([1, 1, 1, 2, 3].removeDuplcatesSet(), [1, 2, 3]);
+  t.deepEqual([1, 2, 2, 2, 3].removeDuplcatesSet(), [1, 2, 3]);
+  t.deepEqual([1, 2, 2, 3, 3].removeDuplcatesSet(), [1, 2, 3]);
+});
 
-  t.is([1, 1, 1, 2, 3].removeDuplcatesInPlace(), 3);
-  t.is([1, 2, 2, 2, 3].removeDuplcatesInPlace(), 3);
-  t.is([1, 2, 2, 3, 3].removeDuplcatesInPlace(), 3);
+test('remove duplicates of sorted array in place', (t) => {
+  let array = [1];
+
+  t.is(array.removeDuplcatesInPlace(), 1);
+  t.deepEqual(array, [1]);
+
+  array = [1, 1];
+
+  t.is(array.removeDuplcatesInPlace(), 1);
+  t.deepEqual(array, [1]);
+
+  array = [1, 1, 1];
+
+  t.is(array.removeDuplcatesInPlace(), 1);
+  t.deepEqual(array, [1]);
+
+  array = [1, 1, 2];
+
+  t.is(array.removeDuplcatesInPlace(), 2);
+  t.deepEqual(array, [1, 2]);
+
+  array = [1, 2, 2];
+
+  t.is(array.removeDuplcatesInPlace(), 2);
+  t.deepEqual(array, [1, 2]);
+
+  array = [1, 1, 2, 3];
+
+  t.is(array.removeDuplcatesInPlace(), 3);
+  t.deepEqual(array, [1, 2, 3]);
+
+
+  array = [1, 2, 2, 3];
+
+  t.is(array.removeDuplcatesInPlace(), 3);
+  t.deepEqual(array, [1, 2, 3]);
+
+  array = [1, 2, 3, 3];
+
+  t.is(array.removeDuplcatesInPlace(), 3);
+  t.deepEqual(array, [1, 2, 3]);
+});
+
+test('list all anagrams', (t) => {
+  const anagrams = ['dog', 'cat', 'ddd', 'goo', 'act', 'god'];
+
+  t.deepEqual(anagrams.listAllAnagrams(), ['dog', 'god', 'cat', 'act']);
 });
